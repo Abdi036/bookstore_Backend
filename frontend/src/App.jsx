44e0c47e1ffbce5ced0c3a0e994de614e21669a1
Abdi Kumela <abdikumela036@gmail.com>
2024-06-30 00:06:54 +0300

    make the card component responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Components/Button";
 import Card from "./Components/Card";
-
+import data from "../public/data.json";
 export default function Header() {
   return (
     <header>
@@ -13,7 +13,11 @@ export default function Header() {
       <div className="p-5 w-full flex justify-end">
         <Button>AddBook</Button>
       </div>
-      <Card />
+      <div className="border w-full h-auto p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {data.map((book) => (
+          <Card key={book.id} title={book.title} author={book.author} />
+        ))}
+      </div>
     </header>
   );
 }
--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ title, author }) {
   return (
-    <div className="border w-full h-auto p-5 cursor-pointer">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <img
-          className="w-full"
-          src="./marvels_spider_man_miles_morales_ps4_and_ps5_video_game_2-wallpaper-1366x768.jpg"
-          alt="Book_image"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Title</div>
-          <p className="text-gray-700 text-base">Author</p>
-        </div>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white cursor-pointer">
+      <img
+        className="w-full"
+        src="./marvels_spider_man_miles_morales_ps4_and_ps5_video_game_2-wallpaper-1366x768.jpg"
+        alt="Book_image"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{author}</p>
       </div>
     </div>
   );
